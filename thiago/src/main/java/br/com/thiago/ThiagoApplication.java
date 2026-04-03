fa22a87c0d9258c82d053d724091d4b0199092cf
thiagosenac-dev <thiagodosenac@gmail.com>
2026-04-03 14:50:25 -0300

    Add files via upload

---
+++ thiago/src/main/java/br/com/thiago/ThiagoApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiagoApplication.class, args);
+	}
+
+}
